fda92a084a68c1c84ae1c3a37e170aaa64a931c2
charlie lucas <charlie lucas>
2017-02-27 15:42:45 +0100

    initial App

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+var trBot = require('../lib');
+
+describe('trBot', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
